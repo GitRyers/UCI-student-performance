4bacefb716f55db2a41215845df10e67cd13adfb
Ryan Nguyen <rnguye14@jh.edu>
2025-04-02 12:43:39 -0400

    math eda

--- src/eda.py
+++ src/eda.py
@@ -4,22 +4,31 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# %%
-mat = pd.read_csv('../data/student-mat.csv', sep=';')
-por = pd.read_csv('../data/student-por.csv', sep=';')
-mat.shape, por.shape
+# %% [markdown]
+# # Math
 
 # %%
+mat = pd.read_csv('../data/student-mat.csv', sep=';', usecols=list(range(15, 30)))
+print(mat.shape)
 mat.head()
 
 # %%
 mat.info()
 
 # %%
-mat.describe()
+# Binary variables
+fig, axes = plt.subplots(2, 4, figsize=(20, 10))
+for i in range(8):
+    sns.histplot(x=mat.columns[i], data=mat, ax=axes[i // 4, i % 4])
+    axes[i // 4, i % 4].set_title(mat.columns[i])
 
 # %%
-por.head()
+# Categorical variables
+fig, axes = plt.subplots(2, 4, figsize=(20, 10))
+for i in range(8, 15):
+    j = i - 8
+    sns.histplot(x=mat.columns[i], data=mat, ax=axes[j // 4, j % 4])
+    axes[j // 4, j % 4].set_title(mat.columns[i])
 
 
 
